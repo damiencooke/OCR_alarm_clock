ca59bc2e1ff8692d1f935f6e367e8331df3d032b
damien.cooke <damien.cooke@digital.hmrc.gov.uk>
2017-03-02 16:36:28 +0000

    interim commit

--- src/test/scala/CanaryTest.scala
+++ src/test/scala/CanaryTest.scala
@@ -1,9 +1,49 @@
-import org.scalatest.{Matchers, FlatSpec}
+import WhoWonCalculator.{Board, PokerCard, PokerHand}
+import org.scalatest.{FlatSpec, Matchers}
 
 class CanaryTest extends FlatSpec with Matchers{
 
-  "2 + 2 " should "equal 4" ignore {
-    2 + 2 should be (5)
-  }
+//  "The who won calculator" should "make AD 10C beat 2C 8S when the board is 6S JD 7C QH 3H" in {
+//
+//    val result = WhoWonCalculator.winningHand(handOne = List(("A", "D"),("10", "C")), handTwo = List(("2", "C"),("8", "S")),
+//                                              board = List(("6", "S"), ("J", "D"), ("7", "C"), ("Q", "H"), ("3", "H")))
+//
+//    result shouldBe "hand one wins"
+//
+//  }
+//
+//  it should "make 2C 8S beat AD 10C when the board is 6S JD 7C QH 3H" in {
+//
+//    val result = WhoWonCalculator.winningHand(handOne = List(("2", "C"),("8", "S")), handTwo = List(("A", "D"),("10", "C")),
+//      board = List(("6", "S"), ("J", "D"), ("7", "C"), ("Q", "H"), ("3", "H")))
+//
+//    result shouldBe "hand two wins"
+//
+//  }
+//
+//  it should "make 2C 8S draw to 2D 8H when the board is 6S JD 7C QH 3H" in {
+//
+//    val result = WhoWonCalculator.winningHand(handOne = List(("2", "C"),("8", "S")), handTwo = List(("2", "D"),("8", "H")),
+//      board = List(("6", "S"), ("J", "D"), ("7", "C"), ("Q", "H"), ("3", "H")))
+//
+//    result shouldBe "it's a draw"
+//
+//  }
 
+  "The who won calculator" should "make 2C 8S beat AD 10C when the board is 2S JD 7C QH 3H" in {
+
+    val result = WhoWonCalculator.winningHand(handOne = List(("2", "C"),("8", "S")), handTwo = List(("A", "D"),("10", "C")),
+      board = List(("2", "S"), ("J", "D"), ("7", "C"), ("Q", "H"), ("3", "H")))
+
+    result shouldBe "hand one wins"
+
+  }
+  "The who won calculator" should "make 2C 8S lose to AD 10C when the board is AC JD 7C QH 3H" in {
+
+    val result = WhoWonCalculator.winningHand(handOne = List(("A", "C"),("8", "S")), handTwo = List(("A", "D"),("10", "C")),
+      board = List(("2", "S"), ("J", "D"), ("7", "C"), ("Q", "H"), ("3", "H")))
+
+    result shouldBe "hand two wins"
+
+  }
 }
