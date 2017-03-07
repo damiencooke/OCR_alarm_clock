3af7a9e083343abf06c349bcfa36b70426cb8894
damien.cooke <damien.cooke@digital.hmrc.gov.uk>
2017-03-07 17:32:12 +0000

    roman-numerals | finished first attempt at roman numerals kata

--- src/test/scala/CanaryTest.scala
+++ src/test/scala/CanaryTest.scala
@@ -1,49 +1,71 @@
-import WhoWonCalculator.{Board, PokerCard, PokerHand}
 import org.scalatest.{FlatSpec, Matchers}
 
 class CanaryTest extends FlatSpec with Matchers{
 
-//  "The who won calculator" should "make AD 10C beat 2C 8S when the board is 6S JD 7C QH 3H" in {
-//
-//    val result = WhoWonCalculator.winningHand(handOne = List(("A", "D"),("10", "C")), handTwo = List(("2", "C"),("8", "S")),
-//                                              board = List(("6", "S"), ("J", "D"), ("7", "C"), ("Q", "H"), ("3", "H")))
-//
-//    result shouldBe "hand one wins"
-//
-//  }
-//
-//  it should "make 2C 8S beat AD 10C when the board is 6S JD 7C QH 3H" in {
-//
-//    val result = WhoWonCalculator.winningHand(handOne = List(("2", "C"),("8", "S")), handTwo = List(("A", "D"),("10", "C")),
-//      board = List(("6", "S"), ("J", "D"), ("7", "C"), ("Q", "H"), ("3", "H")))
-//
-//    result shouldBe "hand two wins"
-//
-//  }
-//
-//  it should "make 2C 8S draw to 2D 8H when the board is 6S JD 7C QH 3H" in {
-//
-//    val result = WhoWonCalculator.winningHand(handOne = List(("2", "C"),("8", "S")), handTwo = List(("2", "D"),("8", "H")),
-//      board = List(("6", "S"), ("J", "D"), ("7", "C"), ("Q", "H"), ("3", "H")))
-//
-//    result shouldBe "it's a draw"
-//
-//  }
+  it should "return I when given 1" in {
 
-  "The who won calculator" should "make 2C 8S beat AD 10C when the board is 2S JD 7C QH 3H" in {
-
-    val result = WhoWonCalculator.winningHand(handOne = List(("2", "C"),("8", "S")), handTwo = List(("A", "D"),("10", "C")),
-      board = List(("2", "S"), ("J", "D"), ("7", "C"), ("Q", "H"), ("3", "H")))
-
-    result shouldBe "hand one wins"
-
+    RomanNumerals.numeralsConverter(1) shouldBe "I"
   }
-  "The who won calculator" should "make 2C 8S lose to AD 10C when the board is AC JD 7C QH 3H" in {
 
-    val result = WhoWonCalculator.winningHand(handOne = List(("A", "C"),("8", "S")), handTwo = List(("A", "D"),("10", "C")),
-      board = List(("2", "S"), ("J", "D"), ("7", "C"), ("Q", "H"), ("3", "H")))
+  it should "return IV when given 4" in {
 
-    result shouldBe "hand two wins"
+    RomanNumerals.numeralsConverter(4) shouldBe "IV"
+  }
+  it should "return V when given 5" in {
 
+    RomanNumerals.numeralsConverter(5) shouldBe "V"
+  }
+  it should "return VII when given 7" in {
+
+    RomanNumerals.numeralsConverter(7) shouldBe "VII"
+  }
+  it should "return IX when given 9" in {
+
+    RomanNumerals.numeralsConverter(9) shouldBe "IX"
+  }
+  it should "return X when given 10" in {
+
+    RomanNumerals.numeralsConverter(10) shouldBe "X"
+  }
+
+  it should "return XI when given 40" in {
+
+    RomanNumerals.numeralsConverter(40) shouldBe "XL"
+  }
+  it should "return L when given 50" in {
+
+    RomanNumerals.numeralsConverter(50) shouldBe "L"
+  }
+  it should "return XL when given 70" in {
+
+    RomanNumerals.numeralsConverter(70) shouldBe "LXX"
+  }
+  it should "return LXX when given 90" in {
+
+    RomanNumerals.numeralsConverter(90) shouldBe "XC"
+  }
+  it should "return C when given 100" in {
+
+    RomanNumerals.numeralsConverter(100) shouldBe "C"
+  }
+  it should "return CD when given 400" in {
+
+    RomanNumerals.numeralsConverter(400) shouldBe "CD"
+  }
+  it should "return D when given 500" in {
+
+    RomanNumerals.numeralsConverter(500) shouldBe "D"
+  }
+  it should "return DCC when given 700" in {
+
+    RomanNumerals.numeralsConverter(700) shouldBe "DCC"
+  }
+  it should "return DCC when given 900" in {
+
+    RomanNumerals.numeralsConverter(900) shouldBe "CM"
+  }
+  it should "return M when given 1000" in {
+
+    RomanNumerals.numeralsConverter(1000) shouldBe "M"
   }
 }
