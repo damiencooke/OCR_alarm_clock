c12686cf1e35e80292ad8897663e20ae4134a9fb
damien.cooke <damien.cooke@digital.hmrc.gov.uk>
2017-03-28 12:57:48 +0100

    master | changed scala version to 2.12

--- src/test/scala/CanaryTest.scala
+++ src/test/scala/CanaryTest.scala
@@ -1,9 +1,9 @@
 import org.scalatest.{Matchers, FlatSpec}
 
-class CanaryTest extends FlatSpec with Matchers{
+class CanaryTest extends FlatSpec with Matchers {
 
-  "2 + 2 " should "equal 4" ignore {
-    2 + 2 should be (5)
+  "2 + 2 " should "equal 4" ignore  {
+    2 + 2 shouldBe 4
   }
 
 }
